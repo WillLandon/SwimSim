fa3a3fffc0cbfaa58674fbf2f3799e49bfc9c206
WillLandon <willlandon350@gmail.com>
2025-11-13 10:16:23 -0500

    changed the entire format of all the files.

---
+++ Backend/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(helmet());
+app.use(cors());
+app.use(morgan('combined'));
+app.use(express.json());
+app.use(express.static('../frontend')); // Serve your frontend
+
+// Basic route
+app.get('/api/health', (req, res) => {
+  res.json({ status: 'OK', message: 'Server is running' });
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
--- main.py
+++
@@ -1,7 +0,0 @@
-##from SwimScraper import SwimScraper as ss
-
-#gets Pitt men roster for 2020
-##pitt_M_roster_2020 = ss.getRoster(team = 'University of Pittsburgh', team_ID = 405, gender = 'M', year = 2020)
-
-#gets list of all meets that Pitt participated in for 2020
-##pitt_meetlist_2020 = ss.getTeamMeetList(team_name = 'University of Pittsburgh', team_ID = 405, year = 2020)
